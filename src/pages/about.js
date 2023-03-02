4552946aed0ee3c2b594dece58338e7fb0141c69
Shaun Axani <shaunaxani@Shauns-iMac-9.local>
2023-03-01 19:42:43 -0500

    edited bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,60 +14,80 @@ const About = ({ data }) => (
 
       <div class={aboutStyles.grid}>
         <div class={aboutStyles.gridItem}>
-          <p>
-            Growing up in and around Toronto, Canada, and studying video and music in undergrad, I came to New York City to pursue a master's degree at the renowned <a href="https://tisch.nyu.edu/itp">ITP</a> at <a href="https://tisch.nyu.edu/">NYU Tisch</a>. I have combined my media and story roots with physical computing, mobile development, augmented reality and code to tell new stories in innovative and exciting ways.
-          </p>
-          <p>
-            I have been an adjunct professor at NYU, FIT, and CUNY, teaching students to code in Python, HTML/CSS/Javascript, and Processing, and in 2017, I relocated to San Francisco to learn best-in-class development techniques on site at Apple.
-          </p>
-          <p>
-            As I improve my web development techniques, I continue have creative technology projects. Lately I've been exploring what is possible with ambisonic sound, messing around with <a href="../experiments/ar-record-box">AR</a>, and using React for <a href="https://www.shiftcollab.com/programs/wybl/">a mental health project</a> and <a href="https://github.com/saxani/shaun-axani-dot-com">this very site</a>.
-          </p>
-          <div className= {aboutStyles.imgWrapper}>
+          <div className= {aboutStyles.imgWrapper} style={{marginBottom: '30px'}}>
             <GatsbyImage image = {data.file.childImageSharp.gatsbyImageData} alt="Photo of Shaun Axani" />
           </div>
+          <p>
+            I did my undergrad in media production in Toronto, Canada, and I sought to become an expert with both audio and video technology. After graduating, I produced everything from corporate videos to live music content and was also a failed rock star, briefly touring Canada in a marginally successful band.
+          </p>
+          <p>
+            I left my homeland for <a href='https://tisch.nyu.edu/itp' target='_blank'>ITP at NYU Tisch</a> in 2014, with the goal of taking my traditional media background and putting an interactive twist on it through code and emerging technology. What I discovered was endless options and inspiration, and I continue to grow with each new job, project, and experiment. ITP demystified technology for me, and I’ve been keen to harness its potential in new and exciting ways ever since. Whether it’s code, hardware design, sensors, virtual reality, 3D printing, fabrication, machine learning, big data or more, I no longer feel intimidated by a new challenge. 
+          </p>
+          <p>
+            My work at ITP and in the year following graduation focused on telling stories. But I extended it beyond screens; I wanted to create non-linear interactivity through mobile phones, tablets, VR headsets, physical space, sensors, or any combination of them all.
+          </p>
+          <p>
+            I was fortunate and had some success with a mixed-media escape room experience ‘Quinn’, which premiered at Tribeca Film Festival, and was nominated for the prestigious <a href='https://futureofstorytelling.org/' target='_blank'>Future of Storytelling Prize</a> alongside companies like NASA and BBC. The room was set up in many different locations in 2016, and hundreds of people were able to participate. That work led to being able to create an immersive theatre experience off-Broadway about climate change (Refuge), which utilized (seemingly) unlimited Arduino and Raspberry Pi’s to make numerous magical interactions for the guests.
+          </p>
+          <p>
+            Seeking to improve my coding skillset, I relocated to San Francisco to work at Apple as a frontend developer working full time on apple.com as a contractor through <a href='https://wearekettle.com/' target='_blank'>Kettle</a>. Working within the Apple campus, I learned a lot about making efficient, robust code and refined my ability to collaborate on a project and properly deploy it. After a couple of years of immense learning, I craved the prototyping, iterative design process of ITP. I was fortunate enough to be the first employee at a lab created by <a href="https://ideo.com/" target="_blank">IDEO</a> for <a href="https://www.orbia.com/" target="_blank">Orbia</a>, a global infrastructure company that has a purpose-driven mandate. My days were spent prototyping web and sensor-based apps, researching emerging technology, doing us- er-centered design with consumers, and preparing polished pitches to motivate the company to invest in projects that are “good for business and good for the world.”
+          </p>
+          <p>
+            Throughout my journey, I’ve tried to teach whenever possible, as I think it’s important that students of all ages grow their technological literacy. I’ve taught various coding classes at NYU, Toronto Metropolitan University, Fashion Institute of Technology, and the City University of New York. 
+          </p>
         </div>
         <div class={[aboutStyles.gridItemSmall, aboutStyles.resumeText].join(' ')}>
           <p>WORK EXPERIENCE</p>
-          <p>Kettle | Front-End Developer<span class={aboutStyles.subText}>Aug. 2017 – Present</span></p>
-          <p>NYU, FIT & CUNY | Adjunct Professor <span class={aboutStyles.subText}>Aug. 2016 – May 2017</span></p>
-          <p><a href="http://storyplex.io">Storyplex</a> | Founder <span class={aboutStyles.subText}>June 2016 – Present</span></p>
-          <p>Interlude/Eko | Creative Intern <span class={aboutStyles.subText}>July 2015 - Sept. 2015</span></p>
-          <p>Ryerson University | RTA School of Media | Production Staff & Technical Instructor <span class={aboutStyles.subText}>Aug. 2009 - Aug. 2014</span></p>
+          <p>IDEO / Orbia | Software Developer<span class={aboutStyles.subText}>Fall 2019 – Summer 2022</span></p>
+          <p>Kettle / Apple | Front-End Developer<span class={aboutStyles.subText}>Summer 2017 – Summer 2019</span></p>
+          <p>NYU, TMU, FIT & CUNY | Adjunct Professor <span class={aboutStyles.subText}>Fall 2016 – Spring 2022</span></p>
+          <p><a href="http://storyplex.io">Storyplex</a> | Founder <span class={aboutStyles.subText}>Spring 2016 – Present</span></p>
+          <p>Interlude/Eko | Creative Intern <span class={aboutStyles.subText}>Summer 2015</span></p>
+          <p>Toronto Metropolitan University | RTA School of Media | Production Staff & Technical Instructor <span class={aboutStyles.subText}>2009 - 2014</span></p>
           <p>EDUCATION</p>
           <p>New York University – Masters of Practical Studies (Major – Interactive Telecommunications)<span class={aboutStyles.subText}>2014 - 2016</span></p>
           <p>Ryerson University - Bachelor of Applied Arts (Major - Radio and Television)<span class={aboutStyles.subText}>2003 - 2007</span></p>
+          <p>Microsoft Certifications - Azure Fundamentals <span class={aboutStyles.subText}>2020</span></p>
         </div>
         <div class={[aboutStyles.gridItemSmallest, aboutStyles.resumeText].join(' ')}>
           <p>PROGRAMMING</p>
           <p>
             HTML/CSS/JavaScript <br />
+            MERN + GraphQL stack (MongoDB, Express, React, Node)
             Python <br />
-            Processing <br />
-            React <br />
+            Processing (Java)<br />
+            Arduino IDE (C++)<br />
+            React Native <br />
             Phonegap <br />
-            GraphQL <br />
             Sass <br />
             Handlebars <br />
             Three.js <br />
-          Node.js <br />
+            AWS <br />
+            Azure <br />
+            Firebase <br />
           </p>
           <p>SOFTWARE</p>
           <p>
             Adobe Premiere <br />
             Adobe After Effects <br />
             Adobe Photoshop <br />
+            Figma <br />
+            Framer <br />
             Pro Tools <br />
             Ableton <br />
             Autopano <br />
             Unity <br />
             Eagle <br />
             Vectorworks <br />
+            Sketchup <br />
+            Easel <br />
+            Lightburn <br />
           </p>
           <p>HARDWARE</p>
           <p>
             Arduino <br />
             Raspberry Pi <br />
+            Numerous sensors <br />
             Video equipment <br />
             Audio recording gear <br />
             Laser cutter <br />
